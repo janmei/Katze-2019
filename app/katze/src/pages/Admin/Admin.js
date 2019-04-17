06b106fbb578554d9f54045f0ffd79efbef10ff3
brandlmax <kontakt@brandl-maximilian.de>
2019-04-17 21:19:07 +0200

    Drawer WIP

--- app/katze/src/pages/Admin/Admin.js
+++ app/katze/src/pages/Admin/Admin.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
 import './Admin.css';
+
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class Admin extends Component {
+  constructor() {
+    super();
+
+    // SET THE SLIDE ID TO GLOBAL STATE
+    this.state = {
+      menu: false,
+    };
+  }
+
+  toggleDrawer = open => () => {
+    this.setState({
+      menu: open,
+    });
+  };
+
   render() {
     return (
       <div className="Admin">
-        Admin View
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+        Admin View <br />
+        <Button onClick={this.toggleDrawer(true)}>Open Menu</Button>
+        {/* TODO: Create an Styled Component out of this with an different width */}
+        {/*  https://material-ui.com/customization/overrides/ */}
+        <SwipeableDrawer
+          id="sideMenu"
+          anchor="right"
+          open={this.state.menu}
+          onClose={this.toggleDrawer(false)}
+          onOpen={this.toggleDrawer(true)}
+        >
+          <Typography component="h2" variant="h5" gutterBottom>
+            Template
+          </Typography>
+          <Divider />
+          <Typography component="h2" variant="h5" gutterBottom>
+            Animation
+          </Typography>
+          <Divider />
+          <Typography component="h2" variant="h5" gutterBottom>
+            Countdown
+          </Typography>
+        </SwipeableDrawer>
       </div>
     );
   }
