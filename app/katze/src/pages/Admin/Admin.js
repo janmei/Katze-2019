e8bd35dae323cd48dddf493062e5e877ab246ac7
brandlmax <kontakt@brandl-maximilian.de>
2019-05-08 18:58:43 +0200

    Dropdowns

--- app/katze/src/pages/Admin/Admin.js
+++ app/katze/src/pages/Admin/Admin.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './Admin.css';
 
-import Icon from '@material-ui/core/Icon';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
+import Divider from '@material-ui/core/Divider';
+
+import FormControl from '@material-ui/core/FormControl';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class Admin extends Component {
   constructor() {
@@ -21,6 +25,7 @@ class Admin extends Component {
     // SET THE SLIDE ID TO GLOBAL STATE
     this.state = {
       menu: false,
+      testSelect: '',
     };
   }
 
@@ -30,15 +35,28 @@ class Admin extends Component {
     });
   };
 
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
   render() {
     return (
       <div className="Admin">
-        Admin View <br />
-        <Button onClick={this.toggleDrawer(true)}>
-          <Icon>menz</Icon>
-        </Button>
-        {/* TODO: Create an Styled Component out of this with an different width */}
-        {/*  https://material-ui.com/customization/overrides/ */}
+        <AppBar position="static">
+          <Toolbar className="topbar">
+            <Typography variant="h6" color="inherit">
+              KATZE - Überkontrollzentrum
+            </Typography>
+            <IconButton
+              color="inherit"
+              aria-label="Menu"
+              onClick={this.toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
         <SwipeableDrawer
           id="sideMenu"
           anchor="right"
@@ -46,18 +64,70 @@ class Admin extends Component {
           onClose={this.toggleDrawer(false)}
           onOpen={this.toggleDrawer(true)}
         >
-          <div className="menuWidth" />
-          <Typography component="h2" variant="h5" gutterBottom>
-            Template
-          </Typography>
-          <Divider />
-          <Typography component="h2" variant="h5" gutterBottom>
-            Animation
-          </Typography>
-          <Divider />
-          <Typography component="h2" variant="h5" gutterBottom>
-            Countdown
-          </Typography>
+          <div className="menuWidth">
+            <Typography component="h2" variant="h5" gutterBottom>
+              Template
+            </Typography>
+            <FormControl className="menuForm" variant="outlined">
+              <InputLabel htmlFor="outlined-age-simple">Template</InputLabel>
+              <Select
+                value={this.state.testSelect}
+                onChange={this.handleChange('testSelect')}
+                input={
+                  <OutlinedInput name="testSelect" id="outlined-age-simple" />
+                }
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={1}>Mocked Template A</MenuItem>
+                <MenuItem value={2}>Mocked Template B</MenuItem>
+                <MenuItem value={3}>Mocked Template C</MenuItem>
+              </Select>
+            </FormControl>
+            <Divider />
+            <Typography component="h2" variant="h5" gutterBottom>
+              Animation
+            </Typography>
+            <FormControl className="menuForm" variant="outlined">
+              <InputLabel htmlFor="outlined-age-simple">Semester</InputLabel>
+              <Select
+                value={this.state.testSelect}
+                onChange={this.handleChange('testSelect')}
+                input={
+                  <OutlinedInput name="testSelect" id="outlined-age-simple" />
+                }
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={1}>Mocked Template A</MenuItem>
+                <MenuItem value={2}>Mocked Template B</MenuItem>
+                <MenuItem value={3}>Mocked Template C</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className="menuForm" variant="outlined">
+              <InputLabel htmlFor="outlined-age-simple">Gruppe</InputLabel>
+              <Select
+                value={this.state.testSelect}
+                onChange={this.handleChange('testSelect')}
+                input={
+                  <OutlinedInput name="testSelect" id="outlined-age-simple" />
+                }
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={1}>Mocked Template A</MenuItem>
+                <MenuItem value={2}>Mocked Template B</MenuItem>
+                <MenuItem value={3}>Mocked Template C</MenuItem>
+              </Select>
+            </FormControl>
+            <Divider />
+            <Typography component="h2" variant="h5" gutterBottom>
+              Countdown
+            </Typography>
+          </div>
         </SwipeableDrawer>
       </div>
     );
