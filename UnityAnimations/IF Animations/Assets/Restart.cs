3589d574d537fd75b406e80c4b5fa3ea12b87fe2
brandlmax <kontakt@brandl-maximilian.de>
2019-05-29 18:35:03 +0200

    Restart Test - Trigger from JS

---
+++ UnityAnimations/IF Animations/Assets/Restart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            RestartTheShit();
+        }
+    }
+
+    void RestartTheShit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
