58404272eef6ed7f992f7cce56f26413f90c5c3a
Philipp Kaltofen <PhilippKaltofen@users.noreply.github.com>
2019-07-06 20:26:31 +0200

    Communication Update

---
+++ UnityAnimations2/UnityAnimations3/Assets/Communicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Communicator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string ChangeText(string headline, string subline, int milliseconds = default(int))
+    {
+        Debug.Log("Headline: " + headline + " - Subline: " + subline + " Countdown: " + milliseconds);
+        return "Funktioniert!";
+    }
+}
