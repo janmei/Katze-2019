d8794dafae01ad3fadb0c9d07384d2b0b564ce39
brandlmax <kontakt@brandl-maximilian.de>
2019-07-06 23:41:39 +0200

    Basic Calendar Styling

--- app/katze/src/components/Titelblob/Titelblob.js
+++ app/katze/src/components/Titelblob/Titelblob.js
@@ -1,100 +1,100 @@
 import React, { Component, Fragment } from 'react';
-import { Calendar, globalizeLocalizer } from 'react-big-calendar'
-import globalize from 'globalize'
-import moment from 'moment'
+import { Calendar, globalizeLocalizer } from 'react-big-calendar';
+import globalize from 'globalize';
+import moment from 'moment';
 import events from './events';
 import './Titelblob.css';
 import Countdown from 'react-countdown-now';
-require('globalize/lib/cultures/globalize.culture.de-DE')
-
+require('globalize/lib/cultures/globalize.culture.de-DE');
 
 const renderer = ({ hours, minutes, seconds, completed }) => {
-	return (
-		<span>
-			{minutes}:{seconds}
-		</span>
-	);
+  return (
+    <span>
+      {minutes}:{seconds}
+    </span>
+  );
 };
 
-const localizer = globalizeLocalizer(globalize)
+const localizer = globalizeLocalizer(globalize);
 
 class Titelblob extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			content: {
-				head: '',
-				sub: ''
-			},
-			countdown: '',
-			countdown_active: false,
-			animation: '',
-			team_layer: {}
-        };
-		this.myRef = React.createRef();
-		
-		this.todayStart = new Date()
-		this.todayStart.setHours(10)
-		this.todayStart.setMinutes(0)
-		this.todayStart.setSeconds(0)
+    this.state = {
+      content: {
+        head: '',
+        sub: '',
+      },
+      countdown: '',
+      countdown_active: false,
+      animation: '',
+      team_layer: {},
+    };
+    this.myRef = React.createRef();
 
-		this.todayEnd = new Date()
-		this.todayEnd.setHours(18)
-		this.todayEnd.setMinutes(0)
-		this.todayEnd.setSeconds(0)
-	}
-    
-	componentDidUpdate() {
-		if (this.state.animation === 'sponsors') {
-			var stage = document.getElementById("sponsors");
-			var fadeComplete = function (e) { stage.appendChild(arr[0]); };
-			var arr = stage.getElementsByTagName("div");
+    this.todayStart = new Date();
+    this.todayStart.setHours(10);
+    this.todayStart.setMinutes(0);
+    this.todayStart.setSeconds(0);
 
-			for (var i = 0; i < arr.length; i++) {
-				arr[i].addEventListener("animationend", fadeComplete, false);
-			}
-		}
-	}
-	
+    this.todayEnd = new Date();
+    this.todayEnd.setHours(18);
+    this.todayEnd.setMinutes(0);
+    this.todayEnd.setSeconds(0);
+  }
 
-	componentWillReceiveProps(next) {
-		this.setState({
-			content: {
-				head: next.data.content.head,
-				sub: next.data.content.sub,
-			},
-			countdown: next.data.content.countdown,
-			countdown_active: next.data.content.countdown_active,
-			animation: next.data.animation,
-			team_layer: next.data.team_layer
-		});
-		if (this.state.countdown_active) {
-			// setCountdown(this.state.content.countdown);
-		}
-	}
+  componentDidUpdate() {
+    if (this.state.animation === 'sponsors') {
+      var stage = document.getElementById('sponsors');
+      var fadeComplete = function(e) {
+        stage.appendChild(arr[0]);
+      };
+      var arr = stage.getElementsByTagName('div');
 
-	checkTime(delta) {
-		// console.log(delta);
-		// check if remaining time is 10 sendonds
-		if (delta.total === 10000) {
-			//trigger something
-		}
-	}
+      for (var i = 0; i < arr.length; i++) {
+        arr[i].addEventListener('animationend', fadeComplete, false);
+      }
+    }
+  }
 
-	render() {
-		return (
-			<div className="InfoLayer">
-				{this.state.countdown_active && (
-					<Countdown
-						date={this.state.countdown}
-						onTick={this.checkTime}
-						renderer={renderer}
-					/>
-                )}
-                
-                <div className="WelcomeLayer">
-{/* 
+  componentWillReceiveProps(next) {
+    this.setState({
+      content: {
+        head: next.data.content.head,
+        sub: next.data.content.sub,
+      },
+      countdown: next.data.content.countdown,
+      countdown_active: next.data.content.countdown_active,
+      animation: next.data.animation,
+      team_layer: next.data.team_layer,
+    });
+    if (this.state.countdown_active) {
+      // setCountdown(this.state.content.countdown);
+    }
+  }
+
+  checkTime(delta) {
+    // console.log(delta);
+    // check if remaining time is 10 sendonds
+    if (delta.total === 10000) {
+      //trigger something
+    }
+  }
+
+  render() {
+    return (
+      <div className="InfoLayer">
+        {this.state.countdown_active && (
+          <Countdown
+            date={this.state.countdown}
+            onTick={this.checkTime}
+            renderer={renderer}
+          />
+        )}
+
+        <div className="WelcomeLayer">
+          {/* 
 				{/* CUSTOM TEXT 
 				{this.state.animation === 'text' && (
 					<div className="TextLayer">
@@ -102,26 +102,56 @@ class Titelblob extends Component {
 						<h6>{this.state.content.sub}</h6>
 					</div>
                 )} */}
-                {/* SPONSORS */}
-				{this.state.animation === 'sponsors' && (
-					<div class="SponsorsLayer">
-						<div id="sponsors" ref="this.myRef">
-							<div><img src={require("../../assets/sponsors/3spin.jpg")} /></div>
-							<div><img src={require("../../assets/sponsors/Asta_h_da.png")} /></div>
-							<div><img src={require("../../assets/sponsors/Designerdock.png")} /></div>
-							<div><img src={require("../../assets/sponsors/Honda.png")} /></div>
-							<div><img src={require("../../assets/sponsors/Hype.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Merck.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Meso.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Qundg.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Schumacher.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Sparkasse_da.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Voba_Solutions.png" )}/></div>
-							<div><img src={require("../../assets/sponsors/Zdf.png" )}/></div>
-						</div>
-					</div>
-				)}
-				{/* TEAM 
+          {/* SPONSORS */}
+          {this.state.animation === 'sponsors' && (
+            <div class="SponsorsLayer">
+              <div id="sponsors" ref="this.myRef">
+                <div>
+                  <img src={require('../../assets/sponsors/3spin.jpg')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Asta_h_da.png')} />
+                </div>
+                <div>
+                  <img
+                    src={require('../../assets/sponsors/Designerdock.png')}
+                  />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Honda.png')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Hype.png')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Merck.png')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Meso.png')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Qundg.png')} />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Schumacher.png')} />
+                </div>
+                <div>
+                  <img
+                    src={require('../../assets/sponsors/Sparkasse_da.png')}
+                  />
+                </div>
+                <div>
+                  <img
+                    src={require('../../assets/sponsors/Voba_Solutions.png')}
+                  />
+                </div>
+                <div>
+                  <img src={require('../../assets/sponsors/Zdf.png')} />
+                </div>
+              </div>
+            </div>
+          )}
+          {/* TEAM 
 				{this.state.animation === 'teams' && (
 					<div className="TextLayer">
 						<h1>{this.state.team_layer.name}</h1>
@@ -137,28 +167,31 @@ class Titelblob extends Component {
 					</div>
 
 				)} */}
-				{/* TIMETABLE */}
-					{this.state.animation === 'timetable' && (
-						<div className="calendar">
-							<Calendar
-								localizer={localizer}
-								events={events}
-								step={60}
-								timeslots={1}
-								defaultView={'day'}
-								views={['day']}
-								toolbar={false}
-								min={new Date(2019, 7, 6, 8)}
-								max={new Date(2019, 7, 6, 21)}
-								scrollToTime={moment().toDate()}
-								culture={'de-DE'}
-								getNow={() => moment().add(1, 'h').toDate()}
-							/>
-						</div>
-							
-				)}
-					
-{/* 
+          {/* TIMETABLE */}
+          {this.state.animation === 'timetable' && (
+            <div className="calendar">
+              <Calendar
+                localizer={localizer}
+                events={events}
+                step={60}
+                // timeslots={2}
+                defaultView={'day'}
+                views={['day']}
+                toolbar={false}
+                min={new Date(2019, 7, 6, 8)}
+                max={new Date(2019, 7, 6, 21)}
+                scrollToTime={moment('2019-07-06 11:00').toDate()}
+                culture={'de-DE'}
+                getNow={() =>
+                  moment('2019-07-06 8:00')
+                    .add(1, 'h')
+                    .toDate()
+                }
+              />
+            </div>
+          )}
+
+          {/* 
 
                     <svg id="blob" 
                         viewBox="394 220 1675 1638"
@@ -177,10 +210,10 @@ class Titelblob extends Component {
                         </path>
                         </g>
                     </svg> */}
-                </div>
-            </div>
-		);
-	}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Titelblob;
