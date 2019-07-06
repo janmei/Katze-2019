5986739863925926783bcfab2530c102c0729d61
Jan Meininghaus <hey@janmeininghaus.com>
2019-07-06 19:42:51 +0200

    Calendar implented;

---
+++ app/katze/src/components/Titelblob/events.js
@@ -0,0 +1,11 @@
+const now = new Date()
+
+export default [
+    {
+        id: 0,
+        title: 'Time fram',
+        allDay: false,
+        start: new Date(2019, 6, 6, 10, 0),
+        end: new Date(2019, 6, 6, 13, 0),
+    },
+]
