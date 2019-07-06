c534022f8ba797aead387913b416bba2d6109327
Jan Meininghaus <hey@janmeininghaus.com>
2019-07-06 21:53:29 +0200

    added communication to unity

--- app/katze/src/components/UnityScene/UnityScene.js
+++ app/katze/src/components/UnityScene/UnityScene.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './UnityScene.css';
 import Unity, { UnityContent } from 'react-unity-webgl';
+import moment from 'moment';
 
 // https://github.com/elraccoone/react-unity-webgl/wiki/Unity-Content
 // SEND MESSAGES
@@ -15,10 +16,11 @@ class UnityScene extends Component {
         head: '',
         sub: ''
       },
-      countdown: '',
+      countdown: '2019-07-06T19:34:16.578Z',
       countdown_active: false,
       animation: '',
-      team_layer: {}
+      team_layer: {},
+      loaded: false
     };
 
     this.unityContent = new UnityContent(
@@ -29,9 +31,37 @@ class UnityScene extends Component {
 
   componentDidMount() {
     // Listener für Scene Switch
+    this.unityContent.on("loaded", () => {
+
+      // Now we can for example hide the loading overlay.
+      console.log("loaded");
+
+      if (this.state.animation === 'text') {
+        if (this.state.countdown_active) {
+          this.unityContent.send(
+            'Communicator',
+            'ChangeText',
+            this.state.content.head + '|' + this.state.content.sub + '|' + 'false'
+          )
+        } else {
+          this.unityContent.send(
+            'Communicator',
+            'ChangeText',
+            this.state.content.head + '|' + this.state.content.sub + '|' + this.getDiff(this.state.countdown)
+          )
+        }
+      }
+    });
   }
 
+  componentDidUpdate() {
+    // this.getDiff(this.state.countdown)
+    
+  }
   componentWillReceiveProps(next) {
+
+    console.log("data");
+    
     this.setState({
       content: {
         head: next.data.content.head,
@@ -45,16 +75,12 @@ class UnityScene extends Component {
     if (this.state.countdown_active) {
       // setCountdown(this.state.content.countdown);
     }
+  }
 
-    if (this.state.animation === 'text') {
-      this.unityContent.send(
-        "Communicator",
-        "ChangeText",
-        "test",
-        "asd",
-        1000000000
-      )
-    }
+  getDiff(time) {
+    const current = moment()
+    const target = moment(time).add(2, 'h')
+    return target.diff(current)
   }
 
   render() {
